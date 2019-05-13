b2c9b83ae3d7cead20376ebad1d4d9b222a3a544
ITOMIZUKI <44338865+ITOMIZUKI@users.noreply.github.com>
2019-05-14 01:55:43 +0900

    Add files via upload

---
+++ copytest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copytest : MonoBehaviour
+{
+    public GameObject Cube;
+    float time = 3;
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Vector3 CreatePoint = new Vector3(0, 0, 0);
+            Instantiate(Cube, CreatePoint, Quaternion.identity);
+            time = 3;
+        }
+    }
+}
+
+/*
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour {
+    public GameObject Cube;
+    float time = 5;
+	void Update () {
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            Instantiate(Cube);
+            time =5;
+        }
+	}
+}
+*/
---
+++ csvextractor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.IO;
+
+
+
+public class csvextractor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StreamReader sr = new StreamReader(
+            @"C: \Users\turara\Desktop\段ボール\HOSHITO\position");
+        {
+            // read a line from csv file
+            string line = sr.ReadLine();
+            // separate line by canma and put them into array
+            string[] values = line.Split(',');
+
+            //put string list factors into general list
+            List<string> lists = new List<string>();
+            lists.AddRange(values);
+
+            //print console
+            foreach (string list in lists)
+            {
+                System.Console.Write("{0}", list);
+            }
+            System.Console.WriteLine();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
